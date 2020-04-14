f650097ae154ccc8263813eeb0514c3d8ec2e030
catsby <clint@ctshryock.com>
2020-04-14 07:47:26 -0500

    actual UUID generation

--- backend.go
+++ backend.go
@@ -1,14 +1,13 @@
+// Package uuid is an example Vault secrets plugin
 package uuid
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"strings"
 
-	"github.com/hashicorp/errwrap"
+	uuid "github.com/hashicorp/go-uuid"
 	"github.com/hashicorp/vault/sdk/framework"
-	"github.com/hashicorp/vault/sdk/helper/jsonutil"
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
@@ -29,7 +28,8 @@ func Factory(ctx context.Context, conf *logical.BackendConfig) (logical.Backend,
 		return nil, fmt.Errorf("configuration passed into backend is nil")
 	}
 
-	b.Backend.Setup(ctx, conf)
+	// Setup always returns  nil
+	_ = b.Backend.Setup(ctx, conf)
 
 	return b, nil
 }
@@ -44,20 +44,20 @@ type backend struct {
 func (b *backend) paths() []*framework.Path {
 	return []*framework.Path{
 		{
-			Pattern: framework.MatchAllRegex("path"),
+			Pattern: framework.MatchAllRegex("new"),
 
-			Fields: map[string]*framework.FieldSchema{
-				"path": {
-					Type:        framework.TypeString,
-					Description: "Specifies the path of the secret.",
-				},
-			},
+			// Fields: map[string]*framework.FieldSchema{
+			// 	"new": {
+			// 		Type:        framework.TypeString,
+			// 		Description: "Generates a new UUID.",
+			// 	},
+			// },
 
 			Operations: map[logical.Operation]framework.OperationHandler{
-				logical.ReadOperation: &framework.PathOperation{
-					Callback: b.handleRead,
-					Summary:  "Retrieve the secret from the map.",
-				},
+				// logical.ReadOperation: &framework.PathOperation{
+				// 	Callback: b.handleRead,
+				// 	Summary:  "Retrieve the secret from the map.",
+				// },
 				logical.UpdateOperation: &framework.PathOperation{
 					Callback: b.handleWrite,
 					Summary:  "Store a secret at the specified location.",
@@ -65,84 +65,43 @@ func (b *backend) paths() []*framework.Path {
 				logical.CreateOperation: &framework.PathOperation{
 					Callback: b.handleWrite,
 				},
-				logical.DeleteOperation: &framework.PathOperation{
-					Callback: b.handleDelete,
-					Summary:  "Deletes the secret at the specified location.",
-				},
+				// logical.DeleteOperation: &framework.PathOperation{
+				// 	Callback: b.handleDelete,
+				// 	Summary:  "Deletes the secret at the specified location.",
+				// },
 			},
 
-			ExistenceCheck: b.handleExistenceCheck,
+			// ExistenceCheck: b.handleExistenceCheck,
 		},
 	}
 }
 
-func (b *backend) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
-	out, err := req.Storage.Get(ctx, req.Path)
-	if err != nil {
-		return false, errwrap.Wrapf("existence check failed: {{err}}", err)
-	}
+// TODO return nil/nil here if this is needed
+// func (b *backend) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+// 	out, err := req.Storage.Get(ctx, req.Path)
+// 	if err != nil {
+// 		return false, errwrap.Wrapf("existence check failed: {{err}}", err)
+// 	}
 
-	return out != nil, nil
-}
-
-func (b *backend) handleRead(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-	if req.ClientToken == "" {
-		return nil, fmt.Errorf("client token empty")
-	}
-
-	path := data.Get("path").(string)
-
-	// Decode the data
-	var rawData map[string]interface{}
-	if err := jsonutil.DecodeJSON(b.store[req.ClientToken+"/"+path], &rawData); err != nil {
-		return nil, errwrap.Wrapf("json decoding failed: {{err}}", err)
-	}
-
-	// Generate the response
-	resp := &logical.Response{
-		Data: rawData,
-	}
-
-	return resp, nil
-}
+// 	return out != nil, nil
+// }
 
 func (b *backend) handleWrite(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
 	if req.ClientToken == "" {
 		return nil, fmt.Errorf("client token empty")
 	}
 
-	// Check to make sure that kv pairs provided
-	if len(req.Data) == 0 {
-		return nil, fmt.Errorf("data must be provided to store in secret")
-	}
-
-	path := data.Get("path").(string)
-
-	// JSON encode the data
-	buf, err := json.Marshal(req.Data)
+	output := make(map[string]interface{})
+	uuidStr, err := uuid.GenerateUUID()
 	if err != nil {
-		return nil, errwrap.Wrapf("json encoding failed: {{err}}", err)
+		return nil, fmt.Errorf("error making UUID: %s", err)
 	}
-
-	// Store kv pairs in map at specified path
-	b.store[req.ClientToken+"/"+path] = buf
-
-	return nil, nil
-}
-
-func (b *backend) handleDelete(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
-	if req.ClientToken == "" {
-		return nil, fmt.Errorf("client token empty")
-	}
-
-	path := data.Get("path").(string)
-
-	// Remove entry for specified path
-	delete(b.store, path)
-
-	return nil, nil
+	output["uuid"] = uuidStr
+	return &logical.Response{
+		Data: output,
+	}, nil
 }
 
 const uuidHelp = `
-The uuid backend is a dummy secrets backend that stores kv pairs in a map.
+The uuid backend is a dummy secrets backend that generates a UUID
 `
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/catsby/vault-plugin-secrets-uuid
 go 1.14
 
 require (
-	github.com/hashicorp/errwrap v1.0.0
-	github.com/hashicorp/vault/api v1.0.4 // indirect
+	github.com/hashicorp/go-hclog v0.8.0
+	github.com/hashicorp/go-uuid v1.0.1
+	github.com/hashicorp/vault/api v1.0.4
 	github.com/hashicorp/vault/sdk v0.1.13
 )
